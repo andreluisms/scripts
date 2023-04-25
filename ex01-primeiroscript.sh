39290d9b524c53e0e00d3d8f7b688090cf45b7b6
Andre Lumesi <andreluis.ms@gmail.com>
2023-04-25 07:49:37 -0300

    Adicionado exemplo01

---
+++ ex01-primeiroscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#echo é usado para exibir uma mensagem em tela
+echo -n "Seu nome de usuário é: "
+whoami
+echo "Hora atual, tempo computador ligado, n usuários logados e carga média 1, 5, 15:"
+uptime
+echo -n "O script está executando do diretório: "
+pwd
